872fe9a54fc134e29ebf54e541f8e9937d508797
a <a@a.com>
2024-05-29 18:02:48 -0700

    ktor with ikvm

---
+++ ktor-with-ikvm/ktor-with-ikvm/Program.cs
@@ -0,0 +1,12 @@
+﻿using io.ktor.samples.kodein;
+
+namespace ktor_with_ikvm;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        KodeinAdvancedApplicationKt.main();
+    }
+}
